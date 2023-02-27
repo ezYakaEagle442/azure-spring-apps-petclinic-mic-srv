4b8bc06f0743b31348d5ff8b81ea64f461d86f37
ezYakaEagle442 <stepin.devops@outlook.com>
2023-02-27 12:56:28 +0100

    add config.js

--- spring-petclinic-ui-asa-e/src/main/resources/static/scripts/app.js
+++ spring-petclinic-ui-asa-e/src/main/resources/static/scripts/app.js
@@ -1,12 +1,16 @@
 'use strict';
+
+import {SPRING_CLOUD_GATEWAY_URL} from './config.js';
+
 /* npm install -g @angular/cli */
 /* App Module */
 var petClinicApp = angular.module('petClinicApp', [
     'ui.router', 'layoutNav', 'layoutFooter', 'layoutWelcome',
     'ownerList', 'ownerDetails', 'ownerForm', 'petForm', 'visits', 'vetList']);
 
+
 petClinicApp.constant('environment', {
-    SPRING_CLOUD_GATEWAY_URL: "$SPRING_CLOUD_GATEWAY_URL"
+    SPRING_CLOUD_GATEWAY_URL
     });
 
 petClinicApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', function(
